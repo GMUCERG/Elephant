4e7a2dbe25b7cd8866ed72b0e24dca7f2297da88
Rich H <rhaeussl@mason.gmu.edu>
2020-11-08 09:45:20 -0500

    Remove _c shares from datapath_lfsr

--- src_rtl/protected/elephant_datapath_lfsr.vhd
+++ src_rtl/protected/elephant_datapath_lfsr.vhd
@@ -26,23 +26,18 @@ entity elephant_datapath_lfsr is
         load_key: in std_logic;
         key_in_a: in std_logic_vector(STATE_SIZE-1 downto 0);
         key_in_b: in std_logic_vector(STATE_SIZE-1 downto 0);
-        key_in_c: in std_logic_vector(STATE_SIZE-1 downto 0);
         ele_lfsr_output_a: out std_logic_vector(STATE_SIZE+16-1 downto 0);
         ele_lfsr_output_b: out std_logic_vector(STATE_SIZE+16-1 downto 0);
-        ele_lfsr_output_c: out std_logic_vector(STATE_SIZE+16-1 downto 0)
     );
 end elephant_datapath_lfsr;
 
 architecture behavioral of elephant_datapath_lfsr is
     signal lfsr_input_a: std_logic_vector(STATE_SIZE+16-1 downto 0);
     signal lfsr_input_b: std_logic_vector(STATE_SIZE+16-1 downto 0);
-    signal lfsr_input_c: std_logic_vector(STATE_SIZE+16-1 downto 0);
     signal lfsr_output_a: std_logic_vector(STATE_SIZE+16-1 downto 0);
     signal lfsr_output_b: std_logic_vector(STATE_SIZE+16-1 downto 0);
-    signal lfsr_output_c: std_logic_vector(STATE_SIZE+16-1 downto 0);
     signal lfsr_temp_rot_a: std_logic_vector(7 downto 0);
     signal lfsr_temp_rot_b: std_logic_vector(7 downto 0);
-    signal lfsr_temp_rot_c: std_logic_vector(7 downto 0);
 begin
     --LFSR output
     --C code
@@ -55,29 +50,21 @@ begin
                        lfsr_output_b(3+16 downto 0+16) &
                        lfsr_output_b(7+16 downto 6+16) &
                        (lfsr_output_b(5+16) xor lfsr_output_b(111+16));
-    lfsr_temp_rot_c <= (lfsr_output_c(4+16)  xor lfsr_output_c(24+16)) &
-                       lfsr_output_c(3+16 downto 0+16) &
-                       lfsr_output_c(7+16 downto 6+16) &
-                       (lfsr_output_c(5+16) xor lfsr_output_c(111+16));
 
 
     lfsr_input_a <= lfsr_temp_rot_a & lfsr_output_a(STATE_SIZE+16-1 downto 8) when
                         load_key = '0' else  key_in_a & x"0000";
     lfsr_input_b <= lfsr_temp_rot_b & lfsr_output_b(STATE_SIZE+16-1 downto 8) when
                         load_key = '0' else  key_in_b & x"0000";
-    lfsr_input_c <= lfsr_temp_rot_c & lfsr_output_c(STATE_SIZE+16-1 downto 8) when
-                        load_key = '0' else  key_in_c & x"0000";
 
     ele_lfsr_output_a <= lfsr_output_a;
     ele_lfsr_output_b <= lfsr_output_b;
-    ele_lfsr_output_c <= lfsr_output_c;
 
     p_lfsr_data: process(clk, en)
     begin
         if rising_edge(clk) and en = '1' then
             lfsr_output_a <= lfsr_input_a;
             lfsr_output_b <= lfsr_input_b;
-            lfsr_output_c <= lfsr_input_c;
         end if;
     end process;
 
